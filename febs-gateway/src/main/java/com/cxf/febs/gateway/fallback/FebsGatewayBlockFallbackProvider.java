f682d0ba063002a24579b175ff91148de1054a6c
sixpence0112 <sixpencecxf@163.com>
2020-10-12 11:23:58 +0800

    Sentinel验证码限流

---
+++ febs-gateway/src/main/java/com/cxf/febs/gateway/fallback/FebsGatewayBlockFallbackProvider.java
@@ -0,0 +1,29 @@
+package com.cxf.febs.gateway.fallback;
+
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.BlockResponse;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackProvider;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 自定义限流异常
+ * @author sixpence
+ * @version 1.0 2020/10/12
+ */
+public class FebsGatewayBlockFallbackProvider implements ZuulBlockFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public BlockResponse fallbackResponse(String route, Throwable throwable) {
+        if (throwable instanceof BlockException) {
+            return new BlockResponse(HttpStatus.TOO_MANY_REQUESTS.value(),
+                    "访问频率超限", route);
+        } else {
+            return new BlockResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "系统异常", route);
+        }
+    }
+}
